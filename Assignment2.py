e5119861d29e0897c76e1fef565e70f4878ddf41
smjbashacs <57228693+smjbashacs@users.noreply.github.com>
2021-02-28 04:38:14 +0530

    add proper comments till image displaying

--- Assignment2.py
+++ Assignment2.py
@@ -1,7 +1,15 @@
 import matplotlib.pyplot as plt
+
+# Reading Brain.jpg image to numpy array
 img_array = plt.imread('Brain.jpg')
+
+# Finding the dimensions of the image
 print("Dimensions of image :", img_array.shape)
+
+# Displaying the image from img_array
 plt.imshow(img_array)
 plt.show()
+
+# Using reverse colormap to display image properly
 plt.imshow(img_array, cmap='Greys_r')
 plt.show()
